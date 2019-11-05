c91444a06e4a999e8c085aceb9348aaf11294fb6
dwald <dwald@>
2019-11-05 13:55:36 +0100

    kata00 with map

--- kata00.py
+++ kata00.py
@@ -2,9 +2,10 @@ def kata00():
     """Formats strings."""
 
     t = (19, 42, 21)
-    kata = "The {} numbers are: {}".format(len(t), ', '.join([str(each) for each in t]))
-    print(kata)
+    # kata = "The {} numbers are: {}".format(len(t), ', '.join([str(each) for each in t]))
+    kata = "The {} numbers are: {}".format(len(t), ', '.join(map(str, t)))
     # print(str(t)[1:-1])
+    print(kata)
 
 
 if __name__ == "__main__":
